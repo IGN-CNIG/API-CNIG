3bd319b13ec4afe2a5c28c6da748bb11e8a64066
isabelvigil <isabelvigil@guadaltel.es>
2019-08-05 09:31:32 +0200

    Arreglo webpack

--- api-ign-js/src/plugins/xylocator/webpack-config/webpack.production.config.js
+++ api-ign-js/src/plugins/xylocator/webpack-config/webpack.production.config.js
@@ -25,8 +25,7 @@ module.exports = {
     extensions: ['.wasm', '.mjs', '.js', '.json', '.css', '.hbs', '.html'],
   },
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.js$/,
         exclude: /(node_modules\/(?!ol)|bower_components)/,
         use: {
@@ -48,15 +47,14 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: [
-          {
+        use: [{
             loader: MiniCssExtractPlugin.loader,
           },
           {
             loader: 'css-loader',
-            options: {
-              minimize: true,
-            },
+            // options: {
+            //   minimize: true,
+            // },
           },
         ],
         exclude: [/node_modules/],
@@ -65,7 +63,8 @@ module.exports = {
         test: /\.(woff|woff2|eot|ttf|svg)$/,
         exclude: /node_modules/,
         loader: 'url-loader?name=fonts/[name].[ext]',
-      }],
+      }
+    ],
   },
   optimization: {
     noEmitOnErrors: true,
