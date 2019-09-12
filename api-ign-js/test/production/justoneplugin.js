b509b54104021ea3dfb61f81658ff24a9572e3fa
isabelvigil <isabelvigil@guadaltel.es>
2019-09-12 18:10:48 +0200

    Tarea #135915 Plugin PredefinedZoom - Se crea el nuevo plugin. - Se adapta al antiguo para parametrizar diferentes bbox.

--- api-ign-js/test/production/justoneplugin.js
+++ api-ign-js/test/production/justoneplugin.js
@@ -2,8 +2,12 @@ const map = M.map({
   container: 'map',
 });
 
-const mp = new M.plugin.MeasureBar({
+const mp = new M.plugin.PredefinedZoom({
   position: 'TR',
+  savedZooms: [{
+    name: 'Zoom a la extensión del mapa',
+    bbox: [-2392173.2372, 3033021.2824, 1966571.8637, 6806768.1648],
+  }],
 });
 
 map.addPlugin(mp);
