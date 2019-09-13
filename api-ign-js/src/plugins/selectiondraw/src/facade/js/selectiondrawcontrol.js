3698f8eb6204c5b3c5299087d14dadf867d8b55b
Emilio Martinez Rivero <emiliomartinezriv@gmail.com>
2019-09-13 07:54:59 +0200

    Tarea #135913
    Primera implementacion de la herramienta SelectionDraw.

---
+++ api-ign-js/src/plugins/selectiondraw/src/facade/js/selectiondrawcontrol.js
@@ -0,0 +1,98 @@
+/**
+ * @module M/control/SelectionDrawControl
+ */
+
+import SelectionDrawImplControl from 'impl/selectiondrawcontrol';
+import template from 'templates/selectiondraw';
+
+export default class SelectionDrawControl extends M.Control {
+  /**
+   * @classdesc
+   * Main constructor of the class. Creates a PluginControl
+   * control
+   *
+   * @constructor
+   * @extends {M.Control}
+   * @api
+   */
+  constructor(options) {
+    const impl = new SelectionDrawImplControl(options.projection);
+    super(impl, 'SelectionDraw');
+    impl.facadeControl = this;
+  }
+
+  /**
+   * This function creates the view
+   *
+   * @public
+   * @function
+   * @param {M.Map} map to add the control
+   * @api
+   */
+  createView(map) {
+    return new Promise((success) => {
+      const html = M.template.compileSync(template);
+      const polygonBtn = html.querySelector('button#polygon');
+      const pointBtn = html.querySelector('button#point');
+      const clearBtn = html.querySelector('button#clear');
+      const drawLayer = map.getLayers().find(l => l.name === '__draw__');
+
+      clearBtn.addEventListener('click', () => {
+        drawLayer.clear();
+      });
+      pointBtn.addEventListener('click', () => {
+        const type = 'Point';
+        const pointActive = this.activeGeometry === type;
+        if (pointActive) {
+          this.deactivate([pointBtn, polygonBtn]);
+        } else {
+          this.deactivate([pointBtn, polygonBtn]);
+          this.activate(type, pointBtn);
+        }
+      });
+      polygonBtn.addEventListener('click', () => {
+        const type = 'Polygon';
+        const polygonActivate = this.activeGeometry === type;
+        if (polygonActivate) {
+          this.deactivate([pointBtn, polygonBtn]);
+        } else {
+          this.deactivate([pointBtn, polygonBtn]);
+          this.activate(type, polygonBtn);
+        }
+      });
+      success(html);
+    });
+  }
+
+  /**
+   * @public
+   * @function
+   * @api
+   */
+  activate(type, btn) {
+    this.activeGeometry = type;
+    this.getImpl().activate(type, btn);
+  }
+
+  /**
+   * @public
+   * @function
+   * @api
+   */
+  deactivate(listBtn) {
+    this.activeGeometry = '';
+    this.getImpl().deactivate(listBtn);
+  }
+
+  /**
+   * This function compares controls
+   *
+   * @public
+   * @function
+   * @param {M.Control} control to compare
+   * @api
+   */
+  equals(control) {
+    return control instanceof SelectionDrawControl;
+  }
+}
