e816b3d63ef1063e9cd2c97aac26de73e1900ee4
isabelvigil <isabelvigil@guadaltel.es>
2019-10-18 14:25:07 +0200

    Tarea #135977: Plugin Overview - Se añade fuente con el icono de botón colapsado. - Se parametriza la posición del plugin. - Se parametrizan las opciones collapsed y collapsible. - Se modifica el estilo para una correcta visualización con posición parametrizada.

--- api-ign-js/src/plugins/overview/src/facade/js/overview.js
+++ api-ign-js/src/plugins/overview/src/facade/js/overview.js
@@ -15,7 +15,7 @@ export default class Overview extends M.Plugin {
    * @param {Object} impl implementation object
    * @api stable
    */
-  constructor() {
+  constructor(options = {}) {
     super();
     /**
      * Facade of the map
@@ -30,6 +30,34 @@ export default class Overview extends M.Plugin {
      * @type {Array<M.Control>}
      */
     this.controls_ = [];
+
+    /**
+     * Position of the plugin
+     * @private
+     * @type {String} - 'TR', 'TL', 'BL', 'BR'
+     */
+    this.position_ = options.position || 'TR';
+
+    /**
+     * Whether plugin is collapsed on the beginning.
+     * @private
+     * @type {Boolean}
+     */
+    this.collapsed_ = options.collapsed !== undefined ? options.collapsed : true;
+
+    /**
+     * Whether plugin is collapsible or not.
+     * @private
+     * @type {Boolean}
+     */
+    this.collapsible_ = options.collapsible !== undefined ? options.collapsible : true;
+
+    /**
+     * Base layer for overview map.
+     * @private
+     * @type {String}
+     */
+    this.baseLayer_ = options.baseLayer;
   }
 
   /**
@@ -41,14 +69,14 @@ export default class Overview extends M.Plugin {
    * @api stable
    */
   addTo(map) {
-    this.control_ = new OverviewControl();
+    this.control_ = new OverviewControl(this.baseLayer_);
     this.controls_.push(this.control_);
     this.map_ = map;
     this.panel_ = new M.ui.Panel('panelOverview', {
-      collapsible: true,
-      collapsed: false,
-      position: M.ui.position.TR,
-      collapsedButtonClass: 'g-cartografia-flecha-izquierda',
+      collapsible: this.collapsible_,
+      collapsed: this.collapsed_,
+      position: M.ui.position[this.position_],
+      collapsedButtonClass: 'overview-mundo',
       className: 'overview-panel',
     });
     this.panel_.addControls(this.controls_);
