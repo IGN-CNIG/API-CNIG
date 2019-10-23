66bfa9d6dd6a8e90a5033a5a1ab440dc466d3993
isabelvigil <isabelvigil@guadaltel.es>
2019-10-23 18:38:51 +0200

    Tarea #135914 Plugin GeometryDraw - Se habilita el dibujo de líneas y polígonos. - Se habilita el cambio de color y grosor para puntos, líneas y polígonos. - Se lleva código de OpenLayers a la implementación y se limpia código innecesario. - Se crea método para la eliminación de geometrías dibujadas.

--- api-ign-js/src/plugins/geometrydraw/src/impl/ol/js/geometrydrawcontrol.js
+++ api-ign-js/src/plugins/geometrydraw/src/impl/ol/js/geometrydrawcontrol.js
@@ -17,6 +17,9 @@ export default class GeometryDrawControl extends M.impl.Control {
 
   /**
    * Creates new OpenLayers vector source
+   * @public
+   * @function
+   * @api
    */
   newVectorSource() {
     return new ol.source.Vector();
@@ -25,6 +28,9 @@ export default class GeometryDrawControl extends M.impl.Control {
   /**
    * Creates new OpenLayers vector source with
    * OpenLayers collections as features
+   * @public
+   * @function
+   * @api
    */
   newVectorSourceWithCollectionFeatures() {
     return new ol.source.Vector({ features: new ol.Collection([]) });
@@ -32,6 +38,9 @@ export default class GeometryDrawControl extends M.impl.Control {
 
   /**
    * Creates new OpenLayers draw interaction
+   * @public
+   * @function
+   * @api
    * @param {OLVectorSource} vectorSource -
    * @param {String} geometry - type of geometry ['Point', 'LineString', 'Polygon']
    */
@@ -44,7 +53,10 @@ export default class GeometryDrawControl extends M.impl.Control {
 
   /**
    * Creates new OpenLayers style
-   * @param {*} options - k
+   * @public
+   * @function
+   * @api
+   * @param {Object} options - colors and measures for the style
    */
   newOLStyle(options) {
     return new ol.style.Style({
@@ -57,6 +69,27 @@ export default class GeometryDrawControl extends M.impl.Control {
     });
   }
 
+  /**
+   * Creates new OpenLayers style for polygon
+   * @public
+   * @function
+   * @api
+   * @param {Object} options - colors and measures for the style
+   */
+  newPolygonStyle(options) {
+    return new ol.style.Style({
+      fill: new ol.style.Fill({ color: options.fillColor }),
+      stroke: new ol.style.Stroke({ color: options.strokeColor, width: options.strokeWidth }),
+    });
+  }
+
+  /**
+   * Creates new style for circle points.
+   * @public
+   * @function
+   * @api
+   * @param {Object} options - colors and measures for the style
+   */
   newOLCircleStyle(options) {
     return new ol.style.Style({
       image: new ol.style.Circle({
@@ -72,18 +105,29 @@ export default class GeometryDrawControl extends M.impl.Control {
     });
   }
 
-  newOLTextStyle(text, font, textFillColor, defaultColor, strokeWidth) {
+  /**
+   * Creates new style for text feature.
+   * @public
+   * @function
+   * @api
+   * @param {Object} options - style colors and measures
+   */
+  newOLTextStyle(options) {
     return new ol.style.Style({
       text: new ol.style.Text({
-        text,
-        font,
-        fill: new ol.style.Fill({ color: textFillColor }),
+        text: options.text,
+        font: options.font,
+        fill: new ol.style.Fill({ color: options.textFillColor }),
       }),
-      stroke: new ol.style.Stroke({ color: defaultColor, width: strokeWidth }),
-      fill: new ol.style.Fill({ color: defaultColor }),
+      stroke: new ol.style.Stroke({ color: options.defaultColor, width: options.strokeWidth }),
+      fill: new ol.style.Fill({ color: options.defaultColor }),
     });
   }
 
+  /**
+   * Creates regular polygon style
+   * @param {Object} options -
+   */
   newRegularShapeStyle(options) {
     return new ol.style.Style({
       image: new ol.style.RegularShape({
@@ -95,15 +139,58 @@ export default class GeometryDrawControl extends M.impl.Control {
     });
   }
 
+  /**
+   * Creates polygon feature from extent.
+   * @public
+   * @function
+   * @api
+   * @param {Array} extent - geometry extent
+   */
   newPolygonFeature(extent) {
     return new ol.Feature({ geometry: ol.geom.Polygon.fromExtent(extent) });
   }
 
-  newSquareStyle(color, width) {
+  newDrawnPolygonFeature(vertices) {
+    return new ol.Feature({ geometry: ol.geom.Polygon(vertices) });
+  }
+
+  /**
+   * Creates OL style for simple line
+   * (just color and width)
+   * @public
+   * @function
+   * @api
+   * @param {String} color - line color
+   * @param {Number} width - line stroke width
+   */
+  newSimpleLineStyle(color, width) {
     return new ol.style.Style({ stroke: new ol.style.Stroke({ color, width }) });
   }
 
+  /**
+   * Clones feature
+   * @public
+   * @function
+   * @param {Object} f - OL feature
+   * @api
+   */
   cloneFeature(f) {
     return new ol.Feature(f);
+  }
+
+  /**
+   * Turns OpenLayers feature into Mapea feature.
+   * @param {OpenLayers Feature} olFeature - recently drawn feature
+   */
+  OLToMapeaFeature(olFeature) {
+    const feature = new M.Feature(olFeature.getId(), {
+      geometry: {
+        coordinates: olFeature.getGeometry().getCoordinates(),
+        type: olFeature.getGeometry().getType(),
+      },
+      properties: olFeature.getProperties(),
+    });
+    feature.getImpl().getOLFeature().setStyle(olFeature.getStyle());
+    return feature;
   }
 }
