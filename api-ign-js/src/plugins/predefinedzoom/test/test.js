b8d9dcde5fc0a90ea0e3bdef37fce7eb04b69152
jesusdiaz <jesusdiaz@guadaltel.com>
2020-01-28 11:01:15 +0100

    Se actualizan los test del plugin predefinedzoom

--- api-ign-js/src/plugins/predefinedzoom/test/test.js
+++ api-ign-js/src/plugins/predefinedzoom/test/test.js
@@ -2,14 +2,18 @@ import PredefinedZoom from 'facade/predefinedzoom';
 
 const map = M.map({
   container: 'mapjs',
+  controls: ['panzoom', 'panzoombar'],
+  bbox: [-2563852.2025329857, 3178130.5783665525, 567008.4760278338, 5443112.600512895],
 });
 
 const mp = new PredefinedZoom({
   position: 'TL',
   savedZooms: [{
     name: 'Zoom a la extensión del mapa',
-    bbox: [-2392173.2372, 3033021.2824, 1966571.8637, 6806768.1648],
+    bbox: [-2563852.2025329857, 3178130.5783665525, 567008.4760278338, 5443112.600512895],
   }],
 });
+
+window.map = map;
 
 map.addPlugin(mp);
