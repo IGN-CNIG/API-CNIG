bdf48f394a61ec18eb316c17a0c5de6c30198b3e
Sergio Torrijos <sergiotorrijos@guadaltel.com>
2020-03-16 14:35:07 +0100

    solución proyección EPSG:4326 y EPSG:4258

--- api-ign-js/src/plugins/georefimage/test/test.js
+++ api-ign-js/src/plugins/georefimage/test/test.js
@@ -20,28 +20,22 @@ const map = M.map({
     }),
   ],*/
 });
-
 const layerinicial = new M.layer.WMS({
   url: 'http://www.ign.es/wms-inspire/unidades-administrativas?',
   name: 'AU.AdministrativeBoundary',
   legend: 'Limite administrativo',
   tiled: false,
 }, {});
-
 const campamentos = new M.layer.GeoJSON({
   name: 'Campamentos',
   url: 'http://geostematicos-sigc.juntadeandalucia.es/geoserver/sepim/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=sepim:campamentos&outputFormat=application/json&',
   extract: true,
 });
-
 const georefimage = new Georefimage({
   collapsed: true,
   collapsible: true,
   position: 'TR',
 });
-
-
 map.addLayers([layerinicial, campamentos]);
 map.addPlugin(georefimage);
-
 window.map = map;
