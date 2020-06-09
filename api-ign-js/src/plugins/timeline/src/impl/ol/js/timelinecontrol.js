7d4f6fb8b1694648c489956ff8918ae15240409a
irenevinas <irenevinas@guadaltel.com>
2020-06-09 11:12:39 +0200

    Tarea #158042 Plugin timeline añadido a API-IGN Correción en apiRest

---
+++ api-ign-js/src/plugins/timeline/src/impl/ol/js/timelinecontrol.js
@@ -0,0 +1,23 @@
+/**
+ * @module M/impl/control/TimelineControl
+ */
+
+
+export default class TimelineControl extends M.impl.Control {
+  /**
+   * This function adds the control to the specified map
+   *
+   * @public
+   * @function
+   * @param {M.Map} map to add the plugin
+   * @param {HTMLElement} html of the plugin
+   * @api stable
+   */
+  addTo(map, html) {
+    // super addTo - don't delete
+    this.map = map;
+    this.olMap = map.getMapImpl();
+
+    super.addTo(map, html);
+  }
+}
