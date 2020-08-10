63ef5e9bf886f0e64a8fc14e687678819460ecd0
jesusdiaz <jesusdiaz@guadaltel.com>
2020-08-10 11:47:38 +0200

    Se integra plugin buffer

---
+++ api-ign-js/src/plugins/buffer/src/facade/js/bufferLayer.js
@@ -0,0 +1,49 @@
+/**
+ * @module M/layer/BufferLayer
+ */
+
+import BufferLayerImpl from 'impl/bufferLayerImpl';
+import { getValue } from './i18n/language';
+
+export default class BufferLayer extends M.Layer {
+  /**
+   * @classdesc
+   * Main constructor of the class. Creates a Draw layer
+   * with parameters specified by the user
+   *
+   * @constructor
+   * @extends {M.Layer}
+   * @api stable
+   */
+  constructor(layer) {
+    const impl = new BufferLayerImpl(layer);
+
+    super({}, impl);
+
+    // checks if the implementation can create KML layers
+    if (M.utils.isUndefined(BufferLayerImpl)) {
+      M.exception(getValue('exception_layer'));
+    }
+
+    this.layer = layer;
+
+    this.type = M.layer.type.GeoJSON;
+  }
+
+  /**
+   * This function checks if an object is equals
+   * to this layer
+   *
+   * @function
+   * @api
+   */
+  equals(obj) {
+    let equals = false;
+
+    if (obj instanceof BufferLayer) {
+      equals = this.name === obj.name;
+    }
+
+    return equals;
+  }
+}
