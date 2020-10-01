2d2a296be41e87d94a5fe356b4a83563e8628aba
Sergio Torrijos <sergiotorrijos@guadaltel.com>
2020-10-01 11:49:27 +0200

    Modificación input escale del rescale

--- api-ign-js/src/plugins/rescale/test/test.js
+++ api-ign-js/src/plugins/rescale/test/test.js
@@ -6,6 +6,10 @@ M.language.setLang('es');
 const map = M.map({
   container: 'mapjs',
   controls: ['scale*true'],
+  zoom: 5,
+  maxZoom: 20,
+  minZoom: 4,
+  center: [-467062.8225, 4683459.6216],
 });
 
 const mp = new Rescale({
