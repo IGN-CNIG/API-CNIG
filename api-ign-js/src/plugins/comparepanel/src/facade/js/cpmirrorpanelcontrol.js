d79645a11d80ad24b2330b90009b9fe80f025d91
jesusdiaz <jesusdiaz@guadaltel.com>
2021-02-15 12:28:13 +0100

    Se cierra barra en comparepanel

--- api-ign-js/src/plugins/comparepanel/src/facade/js/cpmirrorpanelcontrol.js
+++ api-ign-js/src/plugins/comparepanel/src/facade/js/cpmirrorpanelcontrol.js
@@ -332,6 +332,7 @@ export default class CompareMirrorpanel extends M.Control {
     if (this.mapL['B'] !== null) { this.mapL['B'].refresh(); }
     if (this.mapL['C'] !== null) { this.mapL['C'].refresh(); }
     if (this.mapL['D'] !== null) { this.mapL['D'].refresh(); }
+    document.querySelector('#m-cp-mirrorpanel > button').click();
   }
 
   /**
