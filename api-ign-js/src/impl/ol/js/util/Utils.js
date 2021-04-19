aa462f7f0f025ad9cf314c789c1b99e6c7ad83fb
jesusdiaz <jesusdiaz@guadaltel.com>
2021-04-19 09:42:06 +0200

    Se disminuye el nivel máximo de zoom

--- api-ign-js/src/impl/ol/js/util/Utils.js
+++ api-ign-js/src/impl/ol/js/util/Utils.js
@@ -119,7 +119,7 @@ class Utils {
     let newMinZoom;
     let newMaxZoom;
     const generatedResolutions = [];
-    const defaultMaxZoom = 28;
+    const defaultMaxZoom = 20;
     // extent
     if (isNullOrEmpty(extent)) {
       newExtent = projection.getExtent();
