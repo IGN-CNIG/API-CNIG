3ae4122638a5629962d65a5d59f89acc0f64c51d
jesusdiaz <jesusdiaz@guadaltel.com>
2021-09-27 15:28:15 +0200

    Se añade formato de imagen a capas WMS

--- api-ign-js/src/impl/ol/js/layer/WMS.js
+++ api-ign-js/src/impl/ol/js/layer/WMS.js
@@ -138,6 +138,10 @@ class WMS extends LayerBase {
     if (isNullOrEmpty(this.options.animated)) {
       this.options.animated = false; // by default
     }
+
+    // format
+    this.format = isNullOrEmpty(this.options.format) ? 'image/png' : this.options.format;
+
     // styles
     this.styles = this.options.styles || '';
     // sldBody
@@ -384,7 +388,7 @@ class WMS extends LayerBase {
         LAYERS: this.name,
         VERSION: this.version,
         TRANSPARENT: this.transparent,
-        FORMAT: 'image/png',
+        FORMAT: this.format,
         STYLES: this.styles,
       };
 
