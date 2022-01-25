6cfe4cf4a6cb2cfa8f1c4921ac267b1be1a4ad79
Jesús Díaz <jesusdiaz@guadaltel.com>
2022-01-25 12:51:11 +0100

    Se realizan ajustes solicitados por IGN

--- api-ign-js/src/plugins/printermap/src/facade/js/printermapcontrol.js
+++ api-ign-js/src/plugins/printermap/src/facade/js/printermapcontrol.js
@@ -961,6 +961,34 @@ export default class PrinterMapControl extends M.Control {
       return (layer.isVisible() && layer.inRange() && layer.name !== 'cluster_cover' && layer.name !== 'selectLayer' && layer.name !== 'empty_layer');
     });
 
+    if (this.map_.getZoom() === 20) {
+      let contains = false;
+      layers.forEach((l) => {
+        if (l.url !== undefined && l.url === 'https://tms-pnoa-ma.idee.es/1.0.0/pnoa-ma/{z}/{x}/{-y}.jpeg') {
+          contains = true;
+        }
+      });
+
+      if (contains) {
+        layers = layers.filter((l) => {
+          return l.url !== 'https://tms-pnoa-ma.idee.es/1.0.0/pnoa-ma/{z}/{x}/{-y}.jpeg';
+        });
+      }
+    } else if (this.map_.getZoom() < 20) {
+      let contains = false;
+      layers.forEach((l) => {
+        if (l.url !== undefined && l.name !== undefined && l.url === 'https://www.ign.es/wmts/pnoa-ma?' && l.name === 'OI.OrthoimageCoverage') {
+          contains = true;
+        }
+      });
+
+      if (contains) {
+        layers = layers.filter((l) => {
+          return l.url !== 'https://www.ign.es/wmts/pnoa-ma?' && l.name !== 'OI.OrthoimageCoverage';
+        });
+      }
+    }
+
     let numLayersToProc = layers.length;
     const otherLayers = this.getImpl().getParametrizedLayers('IMAGEN', layers);
     if (otherLayers.length > 0) {
@@ -968,28 +996,22 @@ export default class PrinterMapControl extends M.Control {
       numLayersToProc = layers.length;
     }
 
+    layers = layers.sort((a, b) => {
+      const zia = a.getZIndex() !== null ? a.getZIndex() : 0;
+      const zib = b.getZIndex() !== null ? b.getZIndex() : 0;
+      return zia > zib;
+    });
+
     return (new Promise((success, fail) => {
-      let encodedLayers = [];
-      const vectorLayers = [];
-      const wmsLayers = [];
-      const otherBaseLayers = [];
+      const encodedLayers = [];
       layers.forEach((layer) => {
         this.getImpl().encodeLayer(layer).then((encodedLayer) => {
-          // Vector layers must be added after non vector layers.
           if (!M.utils.isNullOrEmpty(encodedLayer)) {
-            if (encodedLayer.type === 'Vector' || encodedLayer.type === 'KML') {
-              vectorLayers.push(encodedLayer);
-            } else if (encodedLayer.type === 'WMS') {
-              wmsLayers.push(encodedLayer);
-            } else {
-              otherBaseLayers.push(encodedLayer);
-            }
+            encodedLayers.push(encodedLayer);
           }
 
           numLayersToProc -= 1;
           if (numLayersToProc === 0) {
-            encodedLayers = encodedLayers.concat(otherBaseLayers)
-              .concat(wmsLayers).concat(vectorLayers);
             // Mapfish requires reverse order
             success(encodedLayers.reverse());
           }
@@ -1008,8 +1030,36 @@ export default class PrinterMapControl extends M.Control {
     // Filters WMS and WMTS visible layers whose resolution is inside map resolutions range
     // and that doesn't have Cluster style.
     let layers = this.map_.getLayers().filter((layer) => {
-      return (layer.isVisible() && layer.inRange() && layer.name !== 'cluster_cover' && layer.name !== 'empty_layer' && ['WMS', 'WMTS'].indexOf(layer.type) > -1);
+      return (layer.isVisible() && layer.inRange() && layer.name !== 'cluster_cover' && layer.name !== 'selectLayer' && layer.name !== 'empty_layer' && ['WMS', 'WMTS', 'TMS', 'XYZ'].indexOf(layer.type) > -1);
     });
+
+    if (this.map_.getZoom() === 20) {
+      let contains = false;
+      layers.forEach((l) => {
+        if (l.url !== undefined && l.url === 'https://tms-pnoa-ma.idee.es/1.0.0/pnoa-ma/{z}/{x}/{-y}.jpeg') {
+          contains = true;
+        }
+      });
+
+      if (contains) {
+        layers = layers.filter((l) => {
+          return l.url !== 'https://tms-pnoa-ma.idee.es/1.0.0/pnoa-ma/{z}/{x}/{-y}.jpeg';
+        });
+      }
+    } else if (this.map_.getZoom() < 20) {
+      let contains = false;
+      layers.forEach((l) => {
+        if (l.url !== undefined && l.name !== undefined && l.url === 'https://www.ign.es/wmts/pnoa-ma?' && l.name === 'OI.OrthoimageCoverage') {
+          contains = true;
+        }
+      });
+
+      if (contains) {
+        layers = layers.filter((l) => {
+          return l.url !== 'https://www.ign.es/wmts/pnoa-ma?' && l.name !== 'OI.OrthoimageCoverage';
+        });
+      }
+    }
 
     const encodedLayersModified = [];
     if (this.projection_.value === 'EPSG:3857') {
@@ -1049,32 +1099,27 @@ export default class PrinterMapControl extends M.Control {
       numLayersToProc = layers.length;
     }
 
+    layers = layers.sort((a, b) => {
+      const zia = a.getZIndex() !== null ? a.getZIndex() : 0;
+      const zib = b.getZIndex() !== null ? b.getZIndex() : 0;
+      return zia > zib;
+    });
+
     return (new Promise((success, fail) => {
-      let encodedLayers = [];
-      const vectorLayers = [];
-      const wmsLayers = [];
-      const otherBaseLayers = [];
+      const encodedLayers = [];
       const BreakException = {};
       layers.forEach((layer) => {
         this.getImpl().encodeLayer(layer).then((encodedLayer) => {
           if (encodedLayer === null) {
             throw BreakException;
           }
-          // Vector layers must be added after non vector layers.
+
           if (!M.utils.isNullOrEmpty(encodedLayer)) {
-            if (encodedLayer.type === 'Vector' || encodedLayer.type === 'KML') {
-              vectorLayers.push(encodedLayer);
-            } else if (encodedLayer.type === 'WMS') {
-              wmsLayers.push(encodedLayer);
-            } else {
-              otherBaseLayers.push(encodedLayer);
-            }
+            encodedLayers.push(encodedLayer);
           }
 
           numLayersToProc -= 1;
           if (numLayersToProc === 0) {
-            encodedLayers = encodedLayers.concat(otherBaseLayers)
-              .concat(wmsLayers).concat(vectorLayers);
             // Mapfish requires reverse order
             success(encodedLayers.reverse());
           }
