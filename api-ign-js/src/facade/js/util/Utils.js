55492950c81bcb1ae45b062c102a941dd9d52bd7
Carmen <carmenmarquez@guadaltel.com>
2023-05-03 16:58:53 +0200

    Se anade funcionalidad draggable a utilidades

--- api-ign-js/src/facade/js/util/Utils.js
+++ api-ign-js/src/facade/js/util/Utils.js
@@ -5,6 +5,7 @@
  */
 import { get as remoteGet } from 'M/util/Remote';
 import chroma from 'chroma-js';
+import Draggabilly from 'draggabilly';
 import * as dynamicImage from 'assets/img/dynamic_legend';
 import { INCHES_PER_UNIT, DOTS_PER_INCH } from '../units';
 import * as WKT from '../geom/WKT';
@@ -1392,6 +1393,34 @@ export const encodeBase64 = (json) => {
 export const decodeBase64 = (base64) => {
   const json = window.atob(base64);
   return json;
+};
+
+/**
+ * Esta función proporciona movimiento a un plugin.
+ *
+ * @function
+ * @param {M.ui.Panel} panel Panel del "plugin"
+ * @api
+ */
+export const draggabillyPlugin = (panel) => {
+  const htmlPanel = panel.getTemplatePanel();
+  const draggable = new Draggabilly(htmlPanel, {
+    containment: '.m-mapea-container',
+  });
+  draggable.enable();
+  const closeButton = htmlPanel.querySelector('.m-panel-btn');
+  if (closeButton) {
+    closeButton.addEventListener('click', () => {
+      if (panel.isCollapsed()) {
+        htmlPanel.style.removeProperty('left');
+        htmlPanel.style.removeProperty('top');
+        htmlPanel.style.position = 'relative';
+        draggable.disable();
+      } else {
+        draggable.enable();
+      }
+    });
+  }
 };
 
 /**
