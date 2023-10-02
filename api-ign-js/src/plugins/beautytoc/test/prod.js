f8315166dc9ebcaab0d29c708b33f091892bab72
Carmen <carmenmarquez@guadaltel.com>
2023-10-02 11:21:29 +0200

    limpiaza del proyecto

--- api-ign-js/src/plugins/beautytoc/test/prod.js
+++
@@ -1,7 +0,0 @@
-M.language.setLang('en');
-
-const map = M.map({
-  container: 'mapjs',
-});
-const mp = new M.plugin.BeautyTOC();
-map.addPlugin(mp);
--- api-ign-js/src/plugins/sharemap/test/prod.js
+++
@@ -1,9 +0,0 @@
-const map = M.map({
-  container: 'mapjs',
-});
-const mp = new M.plugin.ShareMap({
-  baseUrl: 'http://mapea4-sigc.juntadeandalucia.es/',
-});
-map.addPlugin(mp);
-
-window.mp = mp;
