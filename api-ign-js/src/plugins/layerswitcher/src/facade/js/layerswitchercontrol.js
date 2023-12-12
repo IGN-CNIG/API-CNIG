4cdfe74d76c9f70af8e2e4e9272500a88a28d21c
Carmen <carmenmarquez@guadaltel.com>
2023-12-12 18:13:10 +0100

    Se actualiza servicio catalogo

--- api-ign-js/src/plugins/layerswitcher/src/facade/js/layerswitchercontrol.js
+++ api-ign-js/src/plugins/layerswitcher/src/facade/js/layerswitchercontrol.js
@@ -20,7 +20,7 @@ import layerModalTemplate from '../../templates/layermodal';
 import customQueryFiltersTemplate from '../../templates/customqueryfilters';
 
 const CATASTRO = '//ovc.catastro.meh.es/Cartografia/WMS/ServidorWMS.aspx';
-const CODSI_CATALOG = 'https://www.idee.es/csw-inspire-idee/srv/spa/q?_content_type=json&bucket=s101&facet.q=type%2Fservice&fast=index&from=*1&resultType=details&sortBy=title&sortOrder=asc&to=*2';
+const CODSI_CATALOG = 'https://www.idee.es/csw-inspire-idee/srv/spa/q?_content_type=json&bucket=s101&facet.q=type%2Fservice&fast=index&from=*1&keyword=WMS%20or%20Web%20Map%20Service%20or%20WMTS%20or%20Web%20Map%20Tile%20Service%20or%20TMS%20or%20MVT%20or%20Features%20WFS&resultType=details&sortBy=title&sortOrder=asc&to=*2';
 const CODSI_PAGESIZE = 9;
 
 // IDs HTML
