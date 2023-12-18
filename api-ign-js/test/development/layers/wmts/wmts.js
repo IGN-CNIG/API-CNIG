a6dc550d6f8338f7081020ecbb2a22ba1749e955
Carmen <carmenmarquez@guadaltel.com>
2023-12-18 14:03:03 +0100

    Se actualizan test

--- api-ign-js/test/development/layers/wmts/wmts.js
+++ api-ign-js/test/development/layers/wmts/wmts.js
@@ -4,7 +4,7 @@ export const wmts_001 = new WMTS({
   url: 'https://wmts-mapa-lidar.idee.es/lidar',
   name: 'EL.GridCoverageDSM',
   legend: 'Modelo Digital de Superficies LiDAR',
-  matrixSet: 'GoogleMapsCompatible',
+  matrixSet: 'EPSG:4326',
   // visibility: true,
   //   isBase: false,
   //   transparent: true,
@@ -18,3 +18,21 @@ export const wmts_001 = new WMTS({
 }, {});
 
 export const wmts_002 = 'WMTS*http://www.ign.es/wmts/pnoa-ma?*OI.OrthoimageCoverage*GoogleMapsCompatible*imagen*true*image/jpeg*true*true*true';
+
+
+export const wmts_003 = new WMTS({
+  url: 'https://servicios.idee.es/wmts/ocupacion-suelo',
+  name: 'LC.LandCoverSurfaces',
+  legend: 'LC.LandCoverSurfaces l',
+  matrixSet: 'GoogleMapsCompatible',
+  format: 'image/png',
+});
+
+
+export const wmts_004 = new WMTS({
+  url: 'https://www.ign.es/wmts/planos?',
+  name: 'MancelliMadrid',
+  legend: 'MancelliMadrid',
+  matrixSet: 'GoogleMapsCompatible',
+  format: 'image/png',
+});
