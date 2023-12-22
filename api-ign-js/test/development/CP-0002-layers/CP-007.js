7ec376ca79f6d6cd51023ef70c39b264f9662984
Carmen <carmenmarquez@guadaltel.com>
2023-12-22 10:22:35 +0100

    test wfs

---
+++ api-ign-js/test/development/CP-0002-layers/CP-007.js
@@ -0,0 +1,17 @@
+import { map as Mmap } from 'M/mapea';
+import { wfs_001 } from '../layers/wfs/wfs';
+
+
+const mapa = Mmap({
+  container: 'map',
+  // projection: 'EPSG:4326*d',
+  center: [-443273.10081370454, 4757481.749296248],
+  zoom: 6,
+  layers: ['OSM'],
+  controls: ['scale', 'getfeatureinfo'],
+});
+
+mapa.addLayers([wfs_001]);
+
+
+window.mapa = mapa;
