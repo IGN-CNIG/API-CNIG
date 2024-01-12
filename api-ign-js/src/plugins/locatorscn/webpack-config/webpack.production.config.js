73de6cfd285c884e975a40246e79d017aca121e9
Antonio Manuel Rebollar Marquez <rebollar.marquez.antoniomanuel@gmail.com>
2024-01-12 14:01:06 +0100

    versionado locatorscn

--- api-ign-js/src/plugins/locatorscn/webpack-config/webpack.production.config.js
+++ api-ign-js/src/plugins/locatorscn/webpack-config/webpack.production.config.js
@@ -12,6 +12,7 @@ module.exports = {
   mode: 'production',
   entry: {
     'locatorscn.ol.min': path.resolve(__dirname, '..', 'src', 'index.js'),
+    [`locatorscn-${pjson.version}.ol.min`]: path.resolve(__dirname, '..', 'src', 'index.js'),
   },
   output: {
     path: path.resolve(__dirname, '..', 'dist'),
