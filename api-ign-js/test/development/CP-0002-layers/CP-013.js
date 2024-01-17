f32e4b5e987b8d4fe258c09f8a2e0085a83fbb9d
alvaroCodes <aramirezm098@gmail.com>
2024-01-17 17:00:33 +0100

    Test maxExtent - #5739

--- api-ign-js/test/development/CP-0002-layers/CP-013.js
+++ api-ign-js/test/development/CP-0002-layers/CP-013.js
@@ -2,11 +2,20 @@ import { map as Mmap } from 'M/mapea';
 // import { vector_001 } from '../layers/vector/vector';
 // import { wfs_001 } from '../layers/wfs/wfs';
 // import { geojson_001 } from '../layers/geojson/geojson';
-import { kml_001 } from '../layers/kml/kml';
+// import { kml_001 } from '../layers/kml/kml';
 // import { mvt_001 } from '../layers/mvt/mvt';
 // import { ogcAPIFeatures_001 } from '../layers/ogcApiFeatures/ogcApiFeatures';
 // import { mbtileVector_001 } from '../layers/mbTilesVector/mbTilesVector';
 // import { generic_002 } from '../layers/generic/generic';
+
+// ---
+
+// import { tms_001 } from '../layers/tms/tms';
+// import { wms_001 } from '../layers/wms/wms';
+// import { wmts_001 } from '../layers/wmts/wmts';
+// import { xyz_001 } from '../layers/xyz/xyz';
+// import { osm } from '../layers/osm/osm';
+import { mbtile_01 } from '../layers/mbtiles/mbtiles';
 
 
 const mapa = Mmap({
@@ -16,23 +25,86 @@ const mapa = Mmap({
   zoom: 6,
 });
 
-mapa.addLayers([
-  // vector_001,
-  // geojson_001,
-  // wfs_001,
-  kml_001,
-  // mvt_001,
-  // ogcAPIFeatures_001,
-  // mbtileVector_001,
-  // generic_002
-]);
-
 // [-] KML
 // mapa.addLayers(kml_001)
 // console.log(kml_001.getMaxExtent())
 // setTimeout(() => {
 //   kml_001.setMaxExtent([-1259872.4694101033, 4359275.566199489, -85799.71494979598, 4620384.454821652])
 // }, 5000);
+
+// [-] MVT
+// mapa.addLayers(mvt_001)
+// console.log(mvt_001.getMaxExtent())
+// setTimeout(() => {
+//     mvt_001.setMaxExtent([-1259872.4694101033, 4359275.566199489, -85799.71494979598, 4620384.454821652])
+// }, 5000);
+
+// [-] WFS
+// mapa.addLayers(wfs_001)
+// console.log(wfs_001.getMaxExtent())
+// setTimeout(() => {
+//     wfs_001.setMaxExtent([-1259872.4694101033, 4359275.566199489, -85799.71494979598, 4620384.454821652])
+// }, 5000);
+
+// [-] Vector
+// mapa.addLayers(vector_001)
+// console.log(vector_001.getMaxExtent())
+// setTimeout(() => {
+//     vector_001.setMaxExtent([-1259872.4694101033, 4359275.566199489, -85799.71494979598, 4620384.454821652])
+// }, 5000);
+
+// [-] GeoJSON
+// mapa.addLayers(geojson_001)
+// console.log(geojson_001.getMaxExtent())
+// setTimeout(() => {
+//     geojson_001.setMaxExtent([-1259872.4694101033, 4359275.566199489, -85799.71494979598, 4620384.454821652])
+// }, 5000);
+
+// [NO CARGA :(] mbtilesvector
+
+// [NO APLICA ¿?] OGCAPI
+// mapa.addLayers(ogcAPIFeatures_001)
+// console.log(geojson_001.getMaxExtent())
+// setTimeout(() => {
+//     geojson_001.setMaxExtent([-1259872.4694101033, 4359275.566199489, -85799.71494979598, 4620384.454821652])
+// }, 5000);
+
+// [-] GenericVector
+// [ ] Refactorizar ¿?
+// mapa.addLayers(generic_002)
+// console.log(generic_002.getMaxExtent())
+// setTimeout(() => {
+//     generic_002.setMaxExtent([-1259872.4694101033, 4359275.566199489, -85799.71494979598, 4620384.454821652])
+// }, 5000);
+
+// [-] WMS
+// mapa.addLayers(wms_001)
+
+// [-] WMTS
+// mapa.addLayers(wmts_001)
+// console.log(osm.getMaxExtent())
+// setTimeout(() => {
+//     osm.setMaxExtent([-1259872.4694101033, 4359275.566199489, -85799.71494979598, 4620384.454821652])
+// }, 5000);
+
+
+// [??] TMS
+// mapa.addLayers(tms_001)
+
+// [-] XYZ
+// mapa.addLayers(xyz_001)
+
+// [-] OSM
+// [ ] Refactorizar ?¿
+// mapa.addLayers(osm)
+// console.log(osm.getMaxExtent())
+// setTimeout(() => {
+//     osm.setMaxExtent([-1259872.4694101033, 4359275.566199489, -85799.71494979598, 4620384.454821652])
+// }, 5000);
+
+// [-] MBTILES
+// mapa.addLayers(mbtile_01)
+
 
 window.mapa = mapa;
 
