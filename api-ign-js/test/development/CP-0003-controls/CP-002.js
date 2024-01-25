88b89cf31dd8269ca00e1d1a9bb2a905b1f4e0ca
Antonio Manuel Rebollar Marquez <rebollar.marquez.antoniomanuel@gmail.com>
2024-01-25 10:23:55 +0100

    Tests de controles creados

---
+++ api-ign-js/test/development/CP-0003-controls/CP-002.js
@@ -0,0 +1,9 @@
+import { map as Mmap } from 'M/mapea';
+
+const mapa = Mmap({
+  container: 'map',
+  projection: 'EPSG:3857*m',
+  controls: ['scale*false', 'scaleline', 'panzoom', 'panzoombar'],
+  center: [-443273.10081370454, 4757481.749296248],
+  zoom: 6,
+});
---
+++ api-ign-js/test/development/CP-0003-controls/CP-004.js
@@ -0,0 +1,13 @@
+import { map as Mmap } from 'M/mapea';
+import { WMS } from 'M/layer/WMS';
+import { wms_001, wms_002, wms_003 } from '../layers/wms/wms';
+
+const mapa = Mmap({
+  container: 'map',
+  projection: 'EPSG:3857*m',
+  controls: ['getfeatureinfo*true'],
+  center: [-443273.10081370454, 4757481.749296248],
+  zoom: 6,
+});
+
+mapa.addLayers([wms_001, wms_002, wms_003]);
