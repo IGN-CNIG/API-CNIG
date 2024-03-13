0998775da9dd578698d024100a230e4b54c696a1
alvaroCodes <aramirezm098@gmail.com>
2024-03-13 08:25:52 +0100

    Se añade etiqueta meta, plugin comparators - windowsynccontrol

--- api-ign-js/src/plugins/comparators/src/facade/js/windowsynccontrol.js
+++ api-ign-js/src/plugins/comparators/src/facade/js/windowsynccontrol.js
@@ -174,6 +174,11 @@ export default class WindowSyncControl extends M.Control {
        <!DOCTYPE html>
        <html>
        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="mapea" content="yes">
+
            <title>Nueva Ventana</title>
            ${this.linksStyle.join(' ')}
            <style rel="stylesheet">
