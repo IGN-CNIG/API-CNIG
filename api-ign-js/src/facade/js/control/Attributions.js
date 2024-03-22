f513a75ba0759e99bea31cca735b32d044b15895
Carmen <carmenmarquez@guadaltel.com>
2024-03-22 09:45:23 +0100

    se soluciona problema con idiomas en attributions control

--- api-ign-js/src/facade/js/control/Attributions.js
+++ api-ign-js/src/facade/js/control/Attributions.js
@@ -70,7 +70,7 @@ class Attributions extends ControlBase {
     this.urlParam_ = options.urlParam || 'url';
     this.defaultAttribution_ = options.defaultAttribution || 'Instituto Geogr&aacute;fico Nacional';
     this.defaultURL_ = options.defaultURL || 'https://www.ign.es/';
-    this.tooltip_ = options.tooltip || getValue('attributions').tooltip;
+    this.tooltip_ = options.tooltip || getValue('attributionsControl').tooltip;
     this.collectionsAttributions_ = options.collectionsAttributions || [];
 
     this.collectionsAttributions_ = this.collectionsAttributions_.map((attr) => {
@@ -497,7 +497,7 @@ class Attributions extends ControlBase {
    * @api
   */
   getHelp() {
-    const textHelp = getValue('attributions').textHelp;
+    const textHelp = getValue('attributionsControl').textHelp;
     return {
       title: Attributions.NAME,
       content: new Promise((success) => {
