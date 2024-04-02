0f1b78dd4f3fc02e7eb3fb832e0ca6b006769c2b
alvaroCodes <aramirezm098@gmail.com>
2024-04-02 13:05:48 +0200

    versionado stereo

--- api-ign-js/src/plugins/stereoscopic/webpack-config/webpack.production.config.js
+++ api-ign-js/src/plugins/stereoscopic/webpack-config/webpack.production.config.js
@@ -12,6 +12,7 @@ module.exports = {
   mode: 'production',
   entry: {
     'stereoscopic.ol.min': path.resolve(__dirname, '..', 'src', 'index.js'),
+    [`stereoscopic-${pjson.version}.ol.min`]: path.resolve(__dirname, '..', 'src', 'index.js'),
   },
   output: {
     path: path.resolve(__dirname, '..', 'dist'),
