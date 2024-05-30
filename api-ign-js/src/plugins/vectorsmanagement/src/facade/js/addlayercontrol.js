99e8dd8afcbddd2407e5708dba4d7bdfdda3d12b
Laura García de Marina Vázquez <lauragarciademarina@gmail.com>
2024-05-30 12:55:17 +0200

    Añadir toast cuando la nueva capa se ha creado

--- api-ign-js/src/plugins/vectorsmanagement/src/facade/js/addlayercontrol.js
+++ api-ign-js/src/plugins/vectorsmanagement/src/facade/js/addlayercontrol.js
@@ -81,6 +81,7 @@ export default class AddLayerControl extends M.Control {
     btn.style.backgroundColor = color;
     btn.addEventListener('click', () => {
       this.addLayer(inputName.value);
+      M.toast.info(getValue('creationLayer_done'), null, 6000);
       // Seleccionar en el desplegable la capa que acabamos de crear
       const selectionLayer = document.querySelector('#m-selectionlayer');
       selectionLayer.options.selectedIndex = 1;
