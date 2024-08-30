ae1f6dbae3d17c9cb8d489d32f8c3dbcf378a528
albertobuces <gdtelcnig_develop@guadaltel.com>
2024-08-30 11:59:12 +0200

    Se incluye plantilla de plugin

---
+++ api-ign-js/src/plugins/basic/src/facade/js/basiccontrol.js
@@ -0,0 +1,93 @@
+/**
+ * @module M/control/BasicControl
+ */
+
+import BasicImplControl from 'impl/basiccontrol';
+import template from 'templates/basic';
+import { getValue } from './i18n/language';
+
+export default class BasicControl extends M.Control {
+  /**
+   * @classdesc
+   * Main constructor of the class. Creates a PluginControl
+   * control
+   *
+   * @constructor
+   * @extends {M.Control}
+   * @api stable
+   */
+  constructor() {
+    // 1. checks if the implementation can create PluginControl
+    if (M.utils.isUndefined(BasicImplControl)) {
+      M.exception(getValue('exceptions.impl'));
+    }
+    // 2. implementation of this control
+    const impl = new BasicImplControl();
+    super(impl, 'Basic');
+  }
+
+  /**
+   * This function creates the view
+   *
+   * @public
+   * @function
+   * @param {M.Map} map to add the control
+   * @api stable
+   */
+  createView(map) {
+    return new Promise((success, fail) => {
+      const html = M.template.compileSync(template, {
+        vars: {
+          translations: {
+            title: getValue('title'),
+            text: getValue('text'),
+          },
+        },
+      });
+      success(html);
+    });
+  }
+
+  /**
+   * This function is called on the control activation
+   *
+   * @public
+   * @function
+   * @api stable
+   */
+  activate() {}
+
+  /**
+   * This function is called on the control deactivation
+   *
+   * @public
+   * @function
+   * @api stable
+   */
+  deactivate() {}
+
+  /**
+   * This function gets activation button
+   *
+   * @public
+   * @function
+   * @param {HTML} html of control
+   * @api stable
+   */
+  getActivationButton(html) {
+    return html.querySelector('.m-basic button');
+  }
+
+  /**
+   * This function compares controls
+   *
+   * @public
+   * @function
+   * @param {M.Control} control to compare
+   * @api stable
+   */
+  equals(control) {
+    return control instanceof BasicControl;
+  }
+  // Add your own functions
+}
