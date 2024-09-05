b164128272b6f99810fdc84b0e673709e85f48d9
Carmen <carmenmarquez@guadaltel.com>
2024-09-05 07:38:36 +0200

    se soluciona problema con capa vector en constructor

--- api-ign-js/src/impl/ol/js/layer/Vector.js
+++ api-ign-js/src/impl/ol/js/layer/Vector.js
@@ -431,6 +431,32 @@ class Vector extends Layer {
   }
 
   /**
+   * Este método devuelve la extensión de todos los objetos geográficos, se
+   * le puede pasar un filtro. Asíncrono.
+   *
+   * @function
+   * @param {boolean} skipFilter Indica si se filtra por el filtro "skip".
+   * @param {M.Filter} filter Filtro.
+   * @return {Array<number>} Extensión de los objetos geográficos.
+   * @api stable
+   */
+  getFeaturesExtentPromise(skipFilter, filter) {
+    return new Promise((resolve) => {
+      const codeProj = this.map.getProjection().code;
+      if (this.isLoaded() === true) {
+        const features = this.getFeatures(skipFilter, filter);
+        const extent = ImplUtils.getFeaturesExtent(features, codeProj);
+        resolve(extent);
+      } else {
+        this.requestFeatures_().then((features) => {
+          const extent = ImplUtils.getFeaturesExtent(features, codeProj);
+          resolve(extent);
+        });
+      }
+    });
+  }
+
+  /**
    * Este método actualiza la capa.
    * @function
    * @api stable
